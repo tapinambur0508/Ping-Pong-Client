858396ab23b2bf0c6d4574a4d10927c7928a73eb
unknown <dasha.g1917@gmail.com>
2018-06-10 22:29:18 +0300

    Add test for Home Component

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Home from '../components/Home';
+import NavLink from 'react-router-dom/NavLink';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import { StaticRouter } from 'react-router'
+describe('Home Component', () => {
+  it('should be defined', () => {
+    expect(Home).toBeDefined();
+  });
+
+  it('Home Component matches the snapshot', () => {
+    const context = {
+      props: {}
+    };
+    const tree = renderer.create(
+      <StaticRouter location="home" context={context}>
+        <Home />
+      </StaticRouter>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('Count NavLink should be equal two', () => {
+    const wrapper = shallow(<Home />);
+
+    const links = wrapper.find(NavLink);
+    expect(links).toHaveLength(2);
+  });
+})
